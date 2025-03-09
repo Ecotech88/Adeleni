02c96170e98bb857136c5d875cfb98dbfd1d9767
Adelani <adeoyejoel121@gmail.com>
2025-03-09 13:13:39 +0100

    ccnsnge
    fff

--- my-app/src/Component/Navbar.jsx
+++ my-app/src/Component/Navbar.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import logo from '../assets/KevinRushLogo.png'
 import github from '../assets/github.png'
 import linkedin from '../assets/linkedin.png'
 import instagram from '../assets/instagram.png'
 import twitter from '../assets/twitter.png'
-
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
     <nav className=' mb-20 flex items-center justify-between'>
         <div className="flex flex-shrink-0 items-center">
-        <img className='mx-2 w-10' src={logo} alt="logo" />
-        </div>
+         </div>
         <div className='m-8 flex items-center justify-center gap-4'>
             <img className='w-5' src={github} alt="github" />
             <img className='w-5' src={linkedin} alt="linkedin" />
@@ -22,4 +19,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar 
--- my-app/src/Component/Technology.jsx
+++ my-app/src/Component/Technology.jsx
@@ -1,36 +1,86 @@
 import github from '../assets/github.png'
-import linkedin from '../assets/linkedin.png'
-import instagram from '../assets/instagram.png'
-import twitter from '../assets/twitter.png'
+import react from '../assets/physics.png'
+import python from '../assets/python.png'
+import Nextjs from '../assets/Next js.png'
+import figma from '../assets/figm.png'
+import mongodb from '../assets/mongodb.png'
+
+import { motion } from "framer-motion"
+
+const iconVariants= (duration) => ({
+    initial: { y: -10},
+    animate: [10, -10],
+    transition: {
+        duration: duration,
+        ease: "linear",
+        repeatType: "reverse",
+    }
+})
 
 const Technology = () => {
   return ( 
     <div className='border-b border-black pb-24'>
-        <h1 className='text-center my-20 text-4xl'>Technologies</h1>
+        <motion.h1
+         whileInview={{ opacity: 1, y: 0}}
+         initial={{ opacity: 0, y: -100}}
+         transition={{ duration: 1.5 }}
+        className='text-center my-20 text-4xl'>Technologies</motion.h1>
 
-        <div className="flex flex-wrap items-center justify-center gap-4 ">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+        whileInview={{ opacity: 1, x: 0}}
+        initial={{ opacity: 0, x: -100}}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4 ">
+            <motion.div
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <img className='w-10' src={react} alt="" />
+            </motion.div>
+            <motion.div
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <img className='w-10' src={Nextjs} alt="" />
+            </motion.div>
+            <motion.div
+            variants={iconVariants(5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <img className='w-10' src={mongodb} alt="" />
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(6)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <img className='w-10' src={figma} alt="" />
+            </motion.div>
+            <motion.div
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <img className='w-10' src={python} alt="" />
+            </motion.div>
+            <motion.div
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                 <img className='w-10' src={github} alt="" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <img className='w-10' src={linkedin} alt="" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <img className='w-10' src={instagram} alt="" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <img className='w-10' src={twitter} alt="" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVariants(5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                 <img className='w-10' src={github} alt="" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <img className='w-10' src={github} alt="" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <img className='w-10' src={github} alt="" />
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     </div>
   )
 }
